52ed66f4e88fb48594283fb0ae7462ecaef2b2ce
Terrence Keane <terrencekeane@gmail.com>
2016-12-16 19:42:17 -0500

    feat(Graph): Add Graph class

---
+++ src/structures/graph/Graph.js
@@ -0,0 +1,157 @@
+import { Queue } from '../queue/Queue';
+
+let _verticesMap = new Map();
+let _connectionsMap = new Map();
+
+export class Graph {
+    constructor() {
+        _verticesMap.set(this, []);
+        _connectionsMap.set(this, new Map());
+    }
+
+    add(v) {
+        _verticesMap.get(this).push(v);
+        _connectionsMap.get(this).set(v, []);
+    }
+
+    remove(vertex) {
+        let vertArr = _verticesMap.get(this);
+        vertArr.splice(vertArr.indexOf(vertex), 1);
+
+        let connections = _connectionsMap.get(this).get(vertex);
+        for (let i = 0; i < connections.length; i++) {
+            this.disconnect(vertex, connections[i]);
+        }
+    }
+
+    connect(vertex1, vertex2) {
+        _connectionsMap.get(this).get(vertex1).push(vertex2);
+        _connectionsMap.get(this).get(vertex2).push(vertex1);
+    }
+
+    disconnect(vertex1, vertex2) {
+        let arr1 = _connectionsMap.get(this).get(vertex1);
+        arr1.splice(arr1.indexOf(vertex2), 1);
+
+        let arr2 = _connectionsMap.get(this).get(vertex2);
+        arr2.splice(arr2.indexOf(vertex1), 1);
+    }
+
+    isEmpty() {
+        return _verticesMap.get(this).length === 0;
+    }
+
+    toString() {
+        let output = '';
+
+        for (let i = 0; i < _verticesMap.get(this).length; i++) {
+            output += `${_verticesMap.get(this)[i]} ->`;
+            let connections = _connectionsMap.get(this).get(_verticesMap.get(this)[i]);
+            for (let y = 0; y < connections.length; y++) {
+                output += ` ${connections[y]}`;
+            }
+            output += '\n';
+        }
+
+        return output;
+    }
+
+    clear() {
+        _verticesMap.set(this, []);
+        _connectionsMap.set(this, new Map());
+    }
+
+    size() {
+        return _verticesMap.get(this).length;
+    }
+
+    contains(vertex) {
+        let index = _verticesMap.get(this).findIndex((element) => {
+            return  element === vertex;
+        });
+
+        return index >= 0 ? true : false;
+    }
+
+    getVerticesConnectedTo(vertex) {
+        return _connectionsMap.get(this).get(vertex);
+    }
+
+    getAllVertices() {
+        return _verticesMap.get(this);
+    }
+
+    forEach(callback) {
+        let
+            color = [],
+            queue = new Queue();
+
+        // Enqueue the point of origin
+        queue.enqueue(_verticesMap.get(this)[0]);
+
+        // Initialize colors
+        for (let i = 0; i < _verticesMap.get(this).length; i++) {
+            color[_verticesMap.get(this)[i]] = 'white';
+        }
+
+        while (!queue.isEmpty()) {
+            let u         = queue.dequeue(),
+                neighbors = _connectionsMap.get(this).get(u);
+
+            // mark vertex u as visited
+            color[u] = 'grey';
+
+            // for each neighbor of vertex u, if unvisited, visit, add to queue and mark as visited
+            for (let i = 0; i < neighbors.length; i++) {
+                let w = neighbors[i];
+                if (color[w] === 'white') {
+                    color[w] = 'grey';
+                    queue.enqueue(w);
+                }
+            }
+
+            // mark vertex u as discovered
+            color[u] = 'black';
+
+            // execute callback on vertex u
+            if (callback) {
+                callback(u);
+            }
+        }
+    }
+
+    getDistancesFrom(vertex) {
+        let color = [],
+            queue = new Queue(),
+            distances = {};
+
+        queue.enqueue(vertex);
+
+        // Initialize colors
+        for (let i = 0; i < _verticesMap.get(this).length; i++) {
+            color[_verticesMap.get(this)[i]] = 'white';
+        }
+
+        for (let i = 0; i < _verticesMap.get(this).length; i++) {
+            distances[_verticesMap.get(this)[i]] = 0;
+        }
+
+        while (!queue.isEmpty()) {
+            let u         = queue.dequeue(),
+                neighbors = _connectionsMap.get(this).get(u);
+
+            color[u] = 'grey';
+            for (let i = 0; i < neighbors.length; i++) {
+                let w = neighbors[i];
+                if (color[w] === 'white') {
+                    color[w] = 'grey';
+                    distances[w] = distances[u] + 1;
+                    queue.enqueue(w);
+                }
+            }
+
+            color[u] = 'black';
+        }
+        return distances;
+    }
+};
---
+++ src/structures/graph/_tests_/Graph.test.js
@@ -0,0 +1,137 @@
+import { expect } from 'Chai';
+import { Graph } from '../Graph';
+
+describe('Graph Class', () => {
+
+    // Test setup
+    let graph = new Graph();
+    let vertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+    for (let i = 0; i < vertices.length; i++) {
+        graph.add(vertices[i]);
+    }
+    graph.connect('A', 'B');
+    graph.connect('A', 'C');
+    graph.connect('A', 'D');
+    graph.connect('C', 'D');
+    graph.connect('C', 'G');
+    graph.connect('D', 'G');
+    graph.connect('D', 'H');
+    graph.connect('B', 'E');
+    graph.connect('B', 'F');
+    graph.connect('E', 'I');
+
+
+    describe('add(v), connect(v,w), and toString()', () => {
+        it('should add vertices, add edges, and print them out correctly', () => {
+            let expectedResult = 'A -> B C D\nB -> A E F\nC -> A D G\nD -> A C G H\n';
+            expectedResult += 'E -> B I\nF -> B\nG -> C D\nH -> D\nI -> E\n';
+            expect(graph.toString()).to.equal(expectedResult);
+
+        });
+    });
+
+    describe('disconnect(vertex1, vertex2)', () => {
+        it('should disconnect the two given vertices', () => {
+            let newGraph = new Graph();
+            let vertices = ['A', 'B', 'C'];
+            for (let i = 0; i < vertices.length; i++) {
+                newGraph.add(vertices[i]);
+            }
+            newGraph.connect('A', 'B');
+            newGraph.connect('A', 'C');
+            newGraph.disconnect('A', 'B');
+            expect(newGraph.getVerticesConnectedTo('A')).to.eql(['C']);
+        });
+    });
+
+    describe('remove(vertex)', () => {
+        let newGraph = new Graph();
+        let vertices = ['A', 'B', 'C'];
+        for (let i = 0; i < vertices.length; i++) {
+            newGraph.add(vertices[i]);
+        }
+        newGraph.connect('A', 'B');
+        newGraph.connect('A', 'C');
+        newGraph.connect('B', 'C');
+        newGraph.remove('B');
+
+        it('should remove vertex from graph', () => {
+            expect(newGraph.getAllVertices()).to.eql(['A', 'C']);
+        });
+
+        it('should remove vertex as a connection to connected vertices', () => {
+            expect(newGraph.getVerticesConnectedTo('A')).to.eql(['C']);
+        });
+    });
+
+    describe('forEach(callback)', () => {
+        it('should hit all vertices in graph, calling callback for each', () => {
+            let visitedVertices = [];
+
+            graph.forEach((v) => {
+                visitedVertices.push(v);
+            });
+
+            expect(visitedVertices).to.eql(vertices);
+        });
+    });
+
+    describe('isEmpty()', () => {
+        it('should return true if graph is not empty', () => {
+            expect(graph.isEmpty()).to.be.false;
+        });
+
+        it('should return false if graph is empty', () => {
+            let emptyGraph = new Graph();
+            expect(emptyGraph.isEmpty()).to.be.true;
+        });
+    });
+
+    describe('getDistancesFrom(vertex)', () => {
+        it('should return an object containing distances from all other vertices', () => {
+            expect(graph.getDistancesFrom(vertices[0])).to.eql({ A: 0, B: 1, C: 1, D: 1, E: 2, F: 2, G: 2, H: 2, I: 3 });
+        });
+    });
+
+    describe('clear()', () => {
+        it('should remove all vertices from graph', () => {
+            let graphToEmpty = new Graph();
+            let vertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+            for (let i = 0; i < vertices.length; i++) {
+                graphToEmpty.add(vertices[i]);
+            }
+            graphToEmpty.clear();
+            expect(graphToEmpty.isEmpty()).to.be.true;
+        });
+    });
+
+    describe('size()', () => {
+        it('should return number of vertices in graph', () => {
+            expect(graph.size()).to.be.equal(9);
+        });
+    });
+
+    describe('contains(vertex)', () => {
+        it('should return true if vertex is in graph', () => {
+            expect(graph.contains(vertices[0])).to.be.true;
+        });
+
+        it('should return false if vertex is in graph', () => {
+            expect(graph.contains('Z')).to.be.false;
+        });
+    });
+
+    describe('getVerticesConnectedTo()', () => {
+        it('should return array containing vertices connected to given vertex', () => {
+            let connections = graph.getVerticesConnectedTo(vertices[0]);
+            let expectedConnections = ['B','C','D'];
+            expect(connections).to.eql(expectedConnections);
+        });
+    });
+
+    describe('getAllVertices()', () => {
+        it('should return array containing all vertices', () => {
+            expect(graph.getAllVertices()).to.eql(vertices);
+        });
+    });
+});
